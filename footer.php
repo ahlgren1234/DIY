f69b785671ff535febfda7415b22ca5dd4768a90
Peter Ahlgren <ahlgren1234@gmail.com>
2017-12-10 21:14:13 +0100

    Added footer.php code.

--- footer.php
+++ footer.php
@@ -1,4 +1,21 @@
-			<div class="box footer"></div>
+			<div class="box footer">
+				
+				<?php if( is_active_sidebar( 'footer' ) ) : ?>
+					<?php dynamic_sidebar( 'footer' ); ?>
+				<?php else : ?>
+
+					<?php if( has_nav_menu( 'footer' ) ) : ?>
+						<?php wp_nav_menu( array( 'theme_location' => 'footer', 'container' => false ) ); ?>
+					<?php endif; ?>
+
+					<p><?php _e( '&copy;', 'diy' ); ?> <?php echo do_shortcode( '[y]' ); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a> <?php _e( '&bull;', 'diy' ); ?> <a href="<?php bloginfo( 'rss2_url' ); ?>"><?php _e( 'RSS Feed', 'diy' ); ?></a></p>
+
+				<?php endif; ?>
+
+			</div>
+
+			<?php wp_footer(); ?>
+
 		</div>
 	</body>
 </html>
