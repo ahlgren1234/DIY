f1889efd6f989c6b512517f424fe7c8d1890c478
Peter Ahlgren <ahlgren1234@gmail.com>
2017-12-10 19:28:36 +0100

    Header WP Code
    Added the WP code in the header.php

--- header.php
+++ header.php
@@ -1,12 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-	<meta charset="UTF-8">
-	<title>DIY STarter Theme</title>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<title>
+		<?php bloginfo( 'name' ); ?>
+		<?php wp_title( '|' ); ?>
+	</title>
+	<?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 
 	<div class="container">
 		<div class="box header"></div>
+			<h1>
+				<a href="<?php echo home_url(); ?>">
+					<?php bloginfo( 'name' ); ?>
+				</a>
+			</h1>
+			<h2><?php bloginfo( 'description' ); ?></h2>
+
+			<?php if( is_active_sidebar( 'header' ) ) : 
+				dynamic_sidebar( 'header' );
+			else :
+				if( has_nav_menu( 'header' ) ) :
+					wp_nav_menu( array(
+						'theme_location'	=> 'header',
+						'container'			=> false
+					) );
+				endif;
+			endif; ?>
+		</div>
 
 	
