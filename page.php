9c9310ff5f8b5009d9f1ec01812b0f16aeb86b4e
Peter Ahlgren <ahlgren1234@gmail.com>
2017-12-10 19:49:41 +0100

    Added the page.php code.

--- page.php
+++ page.php
@@ -1,1 +1,24 @@
-<div class="box content"></div>
+<?php get_header(); ?>
+
+<div class="box content">
+	
+	<?php if( have_posts() ) : ?>
+		<?php while( have_posts() ) : the_post(); ?>
+
+			<div <?php post_class(); ?>>
+				<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+
+				<?php if( !post_password_required() && !is_attachment() && has_post_thumbnail() ) the_post_thumbnail(); ?>
+
+				<?php the_content(); ?>
+				<?php wp_link_pages(); ?>
+			</div>
+
+		<?php endwhile; else : ?>
+		<?php get_template_part( 'inc/not-found' ); ?>
+	<?php endif; ?>
+
+</div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
