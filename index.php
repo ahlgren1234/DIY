faa8d3e1e7dbc295beec453040b6ceeffcc9ea2e
Peter Ahlgren <ahlgren1234@gmail.com>
2017-12-10 19:38:19 +0100

    Index.php wp code
    Added the loop and stuff in index.php

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-<div class="box content"></div>
+<?php get_header(); ?>
+
+<div class="box content">
+
+	<?php if( have_posts() ) : ?>
+		<?php while( have_posts() ) : the_post(); ?>
+
+			<div <?php post_class(); ?>>
+				<h1>
+					<a href="<?php the_permalink(); ?>">
+						<?php the_title(); ?>
+					</a>
+				</h1>
+				<?php the_excerpt(); ?>
+			</div>
+
+		<?php endwhile; ?>
+
+		<div class="nav nav-post nav-archive">
+			<div class="prev">
+				<?php next_posts_link(); ?>
+			</div>
+			<div class="next">
+				<?php previous_posts_link(); ?>
+			</div>
+		</div>
+
+	<?php else : ?>
+
+		<?php get_template_part( 'inc/not-found' ); ?>
+
+	<?php endif; ?>
+
+</div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
